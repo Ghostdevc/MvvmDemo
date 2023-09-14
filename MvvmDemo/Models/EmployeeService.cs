da8072c299712fab31789e572d645939969a61bb
Ghost <b.basturk.bb@gmail.com>
2023-09-14 11:29:07 +0300

    Initial commit

---
+++ MvvmDemo/Models/EmployeeService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmDemo.Models
+{
+    public class EmployeeService
+    {
+
+        private static List<Employee> employees;
+
+        public EmployeeService()
+        {
+            employees = new List<Employee>()
+            {
+                new Employee(){Id = 101 , Name = "Hasan" , Age = 25}
+            };
+        }
+
+        public List<Employee> GetAll()
+        {
+            return employees;
+        }
+
+        public bool Add(Employee employee)
+        {
+            if (21 < employee.Age && employee.Age < 58)
+            {
+                employees.Add(employee);
+                return true;
+            }
+            else throw new ArgumentException("Invalid age");
+            
+        }
+
+        public bool Update(Employee employeeToUpdate)
+        {
+
+            bool isUpdated = false;
+            foreach (Employee employee in employees)
+            {
+                if (employeeToUpdate.Id == employee.Id)
+                {
+                    employee.Name = employeeToUpdate.Name;
+                    employee.Age = employeeToUpdate.Age;
+                    isUpdated = true;
+                    break;
+                }
+            }
+            return isUpdated;
+
+        }
+
+        public bool Delete(int id)
+        {
+            bool isDeleted = false;
+            foreach (Employee employee in employees)
+            {
+                if (id == employee.Id)
+                {
+                    employees.RemoveAt(id);
+                    isDeleted = true;
+                    break;
+                }
+            }
+            return isDeleted;
+
+        }
+
+        public Employee Search(int id)
+        {
+            return employees.FirstOrDefault(x => x.Id == id);
+        }
+
+    }
+}
---
+++ MvvmDemo/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using MvvmDemo.Models;
+using MvvmDemo.Commands;
+using System.Windows;
+using System.Collections.ObjectModel;
+
+namespace MvvmDemo.ViewModels
+{
+    public class EmployeeViewModel : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged_Implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+        
+        EmployeeService EmployeeService;
+
+        public EmployeeViewModel()
+        {
+            EmployeeService = new EmployeeService();
+            LoadData();
+            CurrentEmployee = new Employee();
+            saveCommand = new RelayCommand(Save);
+            searchCommand = new RelayCommand(Search);
+            updateCommand = new RelayCommand(Update);  
+            deleteCommand = new RelayCommand(Delete);
+        }
+        #region DisplayOperation
+        private ObservableCollection<Employee> employeesList;
+
+        public ObservableCollection<Employee> EmployeesList
+        {
+            get { return employeesList; }
+            set { employeesList = value; OnPropertyChanged("EmployeesList"); }
+        }
+
+        private void LoadData()
+        {
+            EmployeesList = new ObservableCollection<Employee>(EmployeeService.GetAll());
+        }
+        #endregion
+
+        private Employee currentEmployee;
+
+        public Employee CurrentEmployee
+        {
+            get { return currentEmployee; }
+            set { currentEmployee = value; OnPropertyChanged("CurrentEmployee"); }
+        }
+
+        private string message;
+
+        public string Message
+        {
+            get { return message; }
+            set { message = value; OnPropertyChanged("Message"); }
+        }
+
+        #region SaveOperation
+        private RelayCommand saveCommand;
+
+        public RelayCommand SaveCommand
+        {
+            get { return saveCommand; }
+        }
+
+        public void Save()
+        {
+            try
+            {
+                var isSaved = EmployeeService.Add(CurrentEmployee);
+                LoadData();
+                if (isSaved)
+                {
+                    Message = "Employee Saved";
+                }
+                else
+                {
+                    Message = "Save Operation Failed";
+                }
+            }
+            catch (Exception ex)
+            {
+                Message = ex.Message;
+            }
+        }
+        #endregion
+
+        #region SearchOperation
+        private RelayCommand searchCommand;
+
+        public RelayCommand SearchCommand
+        {
+            get { return searchCommand; }
+        }
+
+        public void Search()
+        {
+            try
+            {
+                var ObjEmployee = EmployeeService.Search(CurrentEmployee.Id);
+                if (ObjEmployee != null)
+                {
+                    CurrentEmployee.Name = ObjEmployee.Name;
+                    CurrentEmployee.Age = ObjEmployee.Age;
+                }
+                else
+                {
+                    Message = "Employee Not Found";
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Message = ex.Message;
+            }
+        }
+        #endregion
+
+        #region UpdateOperation
+        private RelayCommand updateCommand;
+
+        public RelayCommand UpdateCommand
+        {
+            get { return updateCommand; }
+        }
+
+        public void Update()
+        {
+
+            try
+            {
+                var isUpdated = EmployeeService.Update(CurrentEmployee);
+
+                if (isUpdated)
+                {
+                    Message = "Employee Updated";
+                    LoadData();
+                }
+                else
+                {
+                    Message = "Update operation failed";
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Message = ex.Message;
+            }
+
+        }
+        #endregion
+
+        #region DeleteOperation
+        private RelayCommand deleteCommand;
+
+        public RelayCommand DeleteCommand
+        {
+            get { return deleteCommand; }
+        }
+
+        public void Delete()
+        {
+            try
+            {
+
+                var isDeleted = EmployeeService.Delete(CurrentEmployee.Id);
+
+                if (isDeleted)
+                {
+                    Message = "Employee Deleted";
+                }
+                else { Message = "Delete Operation Failed"; }
+
+            }
+            catch (Exception ex)
+            {
+
+                Message = ex.Message;
+                
+            }
+        }
+        #endregion
+
+
+
+
+
+
+
+
+
+
+    }
+}
